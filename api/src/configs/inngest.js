9068c989dcb9ca265f9fba730ed8cc25ce386696
EL-ILUMINADO <lawrenceozimana@gmail.com>
2026-01-09 09:20:22 +0100

    db config and tools setup

---
+++ api/src/configs/inngest.js
@@ -0,0 +1,27 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
+
+export const inngest = new Inngest({ id: "lio-ecommerce" });
+
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user-created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name } = event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
+      addresses: [],
+      wishlist: [],
+    };
+
+    await User.create(newUser);
+  }
+);
+
+export const functions = [syncUser];
