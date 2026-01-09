0ea32b230e8c754f493537fc71c133074ff3c031
EL-ILUMINADO <lawrenceozimana@gmail.com>
2026-01-09 09:31:47 +0100

    added a text message

--- api/src/index.js
+++ api/src/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV !== "production") {
   const PORT = ENV.PORT || 3000;
   app.listen(
     PORT,
-    () => console.log(`Server running on port ${PORT}`),
+    () => console.log(`Server running on port ${PORT}. DB connected.`),
     connectDB()
   );
 }
