79aba0fdd821d0c0822731ae2f7ade263219ab50
EL-ILUMINADO <lawrenceozimana@gmail.com>
2026-03-13 15:08:28 +0000

    mobile app set up

---
+++ mobile/app/(auth)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function AuthScreen() {
+  return (
+    <View>
+      <Text className="text-gray-500 text-xl">Auth Screen</Text>
+    </View>
+  );
+}
